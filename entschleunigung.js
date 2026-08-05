1c18f62182b642ce694905353430301c206cbc4f
René Coignard <contact@renecoignard.com>
2026-08-05 18:48:42 +0200

    chore: add entschleunigung.js

---
+++ entschleunigung.js
@@ -0,0 +1,15 @@
+// Holpijp 8'
+$:note("[<[1 1] [6 3]> 2 6 [3 <1,3,5>]]*1").scale("[D:Minor C:Major E:Minor]").transpose(+12).rev().midichan(1).midi('IAC Driver Sweelinq')
+
+
+// Prestant/Bourdon 16' + 8', Holpijp 8' + Fluit 4'
+//$:note("[1 _ 5 _]*1").scale("[D:Major C:Major E:Minor]").transpose(-12).midichan(2).midi('IAC Driver Sweelinq')
+
+// Prestant 8' Roerfluit 8' Speelfluit 3' Gemshoorn + Nassat
+//$:note("<[<3 3 3 3>]*1>").scale("[E:Minor]").transpose(-12).midichan(3).midi('IAC Driver Sweelinq')
+//$:note("<[<3 3 3 3>]*1>, <[<5 5 5 5>]*1>").scale("[E:Minor]").transpose(-12).midichan(3).midi('IAC Driver Sweelinq')
+
+
+//$:note("[5]*1").scale("[E:Minor]").transpose(-12).midichan(3).midi('IAC Driver Sweelinq')
+
+//$:note("[c2 a2]").midichan(5).midi('IAC Driver Sweelinq')
