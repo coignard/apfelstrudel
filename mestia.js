51602abba0d4459d98d34c933d6a581c788f1bcd
René Coignard <contact@renecoignard.com>
2026-08-08 08:54:12 +0200

    refactor: get rid of synth artifacts

--- mestia.js
+++ mestia.js
@@ -5,13 +5,13 @@ const key = "D3:aeolian"
 const harm = "<0 6 5 4>/2"
 
 $: "0/2".add(-7).scale(key).note()
-.legato(1).gain(.9).midichan(5).midi(S)
+.legato(1).midichan(5).midi(S)
 
 $: harm.add("0,2,4").scale(key).note()
-.legato(1).gain(.5).midichan(2).midi(S)
+.legato(1).midichan(2).midi(S)
 
 $: "[4 2 0 2 4 5 4 2]".add(harm).add(7).scale(key).note()
-.clip(.7).gain(.78).midichan(1).midi(S)
+.clip(.7).midichan(1).midi(S)
 
 $: "[4 2 0 2 4 5 4 2]*2".add(harm).add(7).scale(key).note()
-.clip(.7).gain(.78).midichan(2).midi(S)
+.clip(.7).midichan(2).midi(S)
